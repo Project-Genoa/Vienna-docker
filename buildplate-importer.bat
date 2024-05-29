93b8d159859c6686fd7ba40aa09484500a679dae
micheal65536 <>
2024-05-29 19:46:57 +0100

    Pull latest version of buildplate importer before running

--- buildplate-importer.bat
+++ buildplate-importer.bat
@@ -1,2 +1,3 @@
+docker compose --env-file settings.txt pull buildplate-importer --include-deps
 set BUILDPLATE_IMPORTER_PLAYER_ID=%1
 docker compose --env-file settings.txt run --rm buildplate-importer
--- buildplate-importer.sh
+++ buildplate-importer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+docker compose --env-file settings.txt pull buildplate-importer --include-deps
 BUILDPLATE_IMPORTER_PLAYER_ID=$1 docker compose --env-file settings.txt run --rm buildplate-importer
